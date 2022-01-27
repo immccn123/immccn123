fa4f1237fd0a3cf4466ee1901bcd6aa88dec9236
Youxi Luo <immccn123@outlook.com>
2022-01-27 12:22:29 +0800

    Heroku Deloy

--- src/App.vue
+++ src/App.vue
@@ -24,50 +24,12 @@ v-list-item-group a {
     style="
       background: url('https://s2.loli.net/2022/01/22/d2UHFOXm8h59Y13.jpg')
         no-repeat;
+      background-size: cover;
+      background-position: center;
+      background-attachment: fixed;
     "
   >
-    <v-app-bar style="opacity: 0.8">
-      <v-app-bar-nav-icon @click="show = !show"></v-app-bar-nav-icon>
-      <router-link to="/"
-        ><v-toolbar-title>immccn123的小站</v-toolbar-title>
-      </router-link>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      permanent
-      :style="{ width: show ? '256px' : '0px' }"
-      style="opacity: 0.8"
-    >
-      <v-list>
-        <v-list-item-group>
-          <router-link to="/">
-            <v-list-item link>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item>
-          </router-link>
-          <router-link to="/about">
-            <v-list-item link>
-              <v-list-item-title>About</v-list-item-title>
-            </v-list-item>
-          </router-link>
-          <router-link to="/friends">
-            <v-list-item link>
-              <v-list-item-title>Friends</v-list-item-title>
-            </v-list-item>
-          </router-link>
-          <router-link to="/blog">
-            <v-list-item link>
-              <v-list-item-title>Blog</v-list-item-title>
-            </v-list-item>
-          </router-link>
-          <a href="https://sy.immccn123.xyz">
-            <v-list-item link>
-              <v-list-item-title>中国双押大词典</v-list-item-title>
-            </v-list-item>
-          </a>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    <NavBar />
     <v-main :style="{ 'padding-left': show ? '256px' : '0px' }">
       <v-container style="opacity: 0.8">
         <router-view />
@@ -93,8 +55,12 @@ v-list-item-group a {
 </template>
 
 <script>
+import NavBar from "./components/NavBar.vue";
 export default {
   name: "App",
+  components: {
+    NavBar,
+  },
   data: () => ({
     show: false,
   }),
