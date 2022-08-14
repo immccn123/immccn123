7c1bcdded28f0736a1cc976b9ab4450b9a89ade2
Youxi Luo <immccn123@outlook.com>
2022-08-14 03:56:20 +0000

    upd

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,15 +1,25 @@
 <template>
     <a-menu v-model:selectedKeys="current" mode="horizontal">
-    <a-menu-item key="home">
-        <router-link to="/">Home</router-link>
-    </a-menu-item>
-    <a-menu-item key="about">
-        <router-link to="/about">About</router-link>
-    </a-menu-item>
+        <a-menu-item key="home">
+            <router-link to="/">Home</router-link>
+        </a-menu-item>
+        <a-menu-item key="about">
+            <router-link to="/about">About</router-link>
+        </a-menu-item>
+        <a-menu-item key="links">
+            <router-link to="/links">Links</router-link>
+        </a-menu-item>
     </a-menu>
 </template>
 <script>
+import { ref } from 'vue';
 export default {
-    name: "TopBar"
+    name: "TopBar",
+    data() {
+        const current = ref<String>(['home'])
+        return {
+            current
+        }
+    }
 }
 </script>
---
+++ src/views/Links.vue
@@ -0,0 +1,41 @@
+<style>
+v-list-item-content a {
+  color: black;
+  text-decoration: none;
+}
+</style>
+
+<template>
+  <div style="margin: 30px;">
+    <h1>友情链接</h1>
+    <h6>Links</h6>
+    <h6>リンク</h6>
+    <a-list bordered :data-source="links">
+        <template #renderItem="{ links }">
+          <a-list-item>{{ links.title }}</a-list-item>
+        </template>
+    </a-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Links",
+  setup() {
+    return {
+      links: [
+        {
+          title: "Rickyxrc",
+          subtitle: "immccn123同机房的大佬",
+          href: "https://github.com/Rickyxrc",
+        },
+        {
+          title: "成都市新津区成外学校",
+          subtitle: "immccn123的学校",
+          href: "http://www.xjmjxccw.com/#/",
+        },
+      ],
+    };
+  },
+};
+</script>
