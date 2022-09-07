f9fd9bcbf7aae0833d11e581bc7f1426fe385e1f
Imken Luo <me@imken.moe>
2022-09-07 00:51:21 +0000

    Add GI's information

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,11 +14,20 @@
       Phigros/Arcaea/DanceRail3/Cytus II/Deemo I~II/Muse Dash/Orzmic/Lanota/osu!/Malody/Genshin Impact/Honkai Impact
       3rd/Minecraft プレイヤー
     </p>
-    <!-- <h2>关于全站通报</h2>
-    <p>
-      好吧。。。这只是我给他的仇人的一些报复。请不要在意这些细节，只是把它当作一个玩笑即可。
-    </p>
-    <p>其实只有跟immccn123有深仇大很的人才会<del>荣耀</del>上榜。</p>
-    <p><del>但愿你不会上榜。/doge</del></p> -->
+    <a-button
+      @click="jump('https://immccn123.notion.site/Genshin-Impact-fad1ef25f3cc4372a8fa6af52f8bc4ab')">Look my
+      my Genshin Impact
+      activities</a-button>
   </div>
 </template>
+<script>
+export default {
+  setup() {
+    return {
+      jump(url) {
+        window.location = url;
+      }
+    }
+  }
+}
+</script>
