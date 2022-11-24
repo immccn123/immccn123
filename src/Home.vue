d539a3975908a5a9945e0dd91db3457934888c53
Imken Luo <me@imken.moe>
2022-11-24 13:08:23 +0000

    Release v3 and say goodbye to old version

---
+++ src/Home.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { NSpace } from 'naive-ui'
+</script>
+
+<template>
+  <div>
+    <n-space vertical>
+      <n-space justify="center">
+        <img src="https://avatars.githubusercontent.com/u/41335471?v=4" alt="avatar">
+        <div>
+          <h1>immccn123 (aka Imken)</h1>
+          <h2>About Me</h2>
+          <p>OIer / Unity & JS & TS Developer</p>
+          <p>Rhythm Game Player</p>
+          <router-link to="/friends">Friend Link</router-link>
+        </div>
+      </n-space>
+      <p style="text-align: center;">
+        <a target="_blank" href="https://github.com/immccn123">GitHub </a>
+        <a target="_blank" href="https://blog.immccn123.xyz/">Blog </a>
+        <a target="_blank" href="https://t.me/immccn123">Telegram </a>
+        <a target="_blank" href="mailto:me@immccn123.xyz">Mail </a><br>
+        <a target="_blank" href="https://discord.gg/JGh6FBtM6Q">Discord (immccn123#7749)</a><br>
+        <a target="_blank" href="https://immccn123.notion.site/Genshin-Impact-fad1ef25f3cc4372a8fa6af52f8bc4ab">Genshin Impact UID:
+          844010126 (Asia)</a> <br>
+        </p>
+        <n-space justify="center">
+          <a target="_blank" href="https://icp.gov.moe/?keyword=20222616">萌ICP备20222616号</a>
+        </n-space>
+    </n-space>
+  </div>
+</template>
+
